d6e654fdcbf39c897c23d3121d9d4d11937fb092
CyberDack <afntwrk.guyrpg@gmail.com>
2021-06-19 16:28:24 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,134 @@
+/**
+ * Stores the list of kittens
+ * @type {Kitten[]}
+ */
+let kittens = [];
+/**
+ * Called when submitting the new Kitten Form
+ * This method will pull data from the form
+ * use the provided function to give the data an id
+ * you can use robohash for images
+ * https://robohash.org/<INSERTCATNAMEHERE>?set=set4
+ * then add that data to the kittens list.
+ * Then reset the form
+ */
+function addKitten(event) {
+  event.preventDefault()
+  let form = event.target
+
+  let newKitten ={
+    id: generateId(),
+    name: form.name.value,
+    image: `https://robohash.org/${form.name.value}?set=set4`
+  }
+
+  kittens.push(newKitten)
+  console.log(kittens)
+  saveKittens()
+
+  form.reset()
+}
+
+/**
+ * Converts the kittens array to a JSON string then
+ * Saves the string to localstorage at the key kittens
+ */
+function saveKittens() {
+  window.localStorage.setItem("kittens",JSON.stringify(kittens))
+  drawKittens()
+}
+
+/**
+ * Attempts to retrieve the kittens string from localstorage
+ * then parses the JSON string into an array. Finally sets
+ * the kittens array to the retrieved array
+ */
+function loadKittens() {
+  let kittensData = JSON.parse(localStorage.getItem("kittens"))
+  if (kittensData){
+    kittens = kittensData
+  }
+  drawKittens()
+}
+
+/**
+ * Draw all of the kittens to the kittens element
+ */
+function drawKittens() {
+  let template = ""
+  kittens.forEach(kitten => {
+    console.log(kitten.image)
+    template += `
+
+    <div class="card p-2 text-center bg-dark kitten">
+    <img class="kitten" src=${kitten.image} height="150" width="150" alt="Missing Kitten">
+    <div class="mt-2 text-light">
+
+      <div class="d-flex justify-content-center"> Name: ${kitten.name}</div>
+    </div>
+
+    <div>
+    </div>
+    `
+  document.getElementById("kittens").innerHTML = template
+  })
+}
+
+/**
+ * Find the kitten in the array by its id
+ * @param {string} id
+ * @return {Kitten}
+ */
+function findKittenById(id) {
+  return kittens.find(k => k.id == id);
+}
+
+/**
+ * Find the kitten in the array of kittens
+ * Generate a random Number
+ * if the number is greater than .7
+ * increase the kittens affection
+ * otherwise decrease the affection
+ * save the kittens
+ * @param {string} id
+ */
+function pet(id) {}
+
+/**
+ * Find the kitten in the array of kittens
+ * Set the kitten's mood to tolerant
+ * Set the kitten's affection to 5
+ * save the kittens
+ * @param {string} id
+ */
+function catnip(id) {}
+
+/**
+ * Sets the kittens mood based on its affection
+ * Happy > 6, Tolerant <= 5, Angry <= 3, Gone <= 0
+ * @param {Kitten} kitten
+ */
+function setKittenMood(kitten) {}
+
+function getStarted() {
+  document.getElementById("welcome").remove();
+  drawKittens();
+}
+
+/**
+ * Defines the Properties of a Kitten
+ * @typedef {{id: string, name: string, mood: string, affection: number}} Kitten
+ */
+
+/**
+ * Used to generate a random string id for mocked
+ * database generated Id
+ * @returns {string}
+ */
+function generateId() {
+  return (
+    Math.floor(Math.random() * 10000000) +
+    "-" +
+    Math.floor(Math.random() * 10000000)
+  );
+}
